dba443cb984a2ff02aad9e15c5f8ea70ffc9f8f5
Wolfgang Figl <wolfgang.figl@me.com>
2019-10-12 12:46:29 +0200

    besserwirdsnicht

--- script/myscript.js
+++ script/myscript.js
@@ -1,3 +1,67 @@
+var movies=
+
+
+[
+  { 
+    "id" : "1",
+    "title": "Muttertag",
+    "Description": "Familie Neugebauer bereitet sich auf den bevorstehenden Muttertag vor. Dabei geht jedes Mitglied auf seine ganz eigene Art und Weise vor.",
+    "Director": "Harald Sicheritz",
+    "Likes": "10",
+    "imagesrc":"./img/muttertag.jpg"
+
+
+},
+{
+  "id" : "2",
+    "title": "Contact High",
+    "Description": "Carlos benötigt dringend eine Tasche, die sich in Polen befindet.Er beauftragt den Autohändler Harry mit der Abholung des wertvollen Gepäckstücks.",
+    "Director": "Michael Glawogger",
+    "Likes": "6",
+    "imagesrc":"./img/contact.jpg"
+},
+
+{
+  "id" : "3",
+    "title": "Indien",
+    "Description": "Der Beamte Heinzi Bösel, ein kleinbürgerlicher Widerling, bekommt als neuen Kollegen ausgerechnet den geschwätzigen, yuppiehaften Streber Kurt Fellner zur Seite gestellt.",
+    "Director": "Paul Harather",
+    "Likes": "7",
+    "imagesrc": "./img/indien.jpg"
+},
+
+{
+  "id" : "4",
+    "title": "Die nackte Kanone",
+    "Description": "Leutnant Frank Drebin ist aus seinem Urlaub in Beirut zurück in Los Angeles und erfährt, dass sein Kollege Nordberg im Krankenhaus liegt,Sein letzter Fall war ein Drogendeal.",
+    "Director": "David Zucker",
+    "Likes": "7",
+    "imagesrc": "./img/kanone.jpg"
+},
+
+{
+  "id" : "5",
+    "title": "El Camino",
+    "Description": "Ein „Breaking Bad“-Film ist ein US-amerikanischer Film, der von Vince Gilligan geschrieben und inszeniert wurde. Er setzt die Handlung aus der Serie Breaking Bad fort.",
+    "Director": "Vince Gilligan",
+    "Likes": "8",
+    "imagesrc": "./img/camino.jpg"
+},
+
+{
+  "id" : "6",
+    "title": "Joker",
+    "Description": "Gotham City in den 80er Jahren: Der Joker war einst ein Comedian. Doch der erhoffte Erfolg blieb aus. Über dunkle Abgründe führte inh sein Weg immer weiter hin zum irren Clown.",
+    "Director": "Todd Phillips",
+    "Likes": "10",
+    "imagesrc": "./img/joker.jpg"
+}
+
+
+]
+
+
+
 
 
 //header//
@@ -6,12 +70,14 @@ var header = document.createElement("header");
 header.className = "header";
 var title = document.createElement("h1");
 title.innerText = "Movie Database";
+
 header.appendChild(title);
 document.body.appendChild(header);
 
 //Nav//
 
 var nav = document.createElement("nav");
+
 nav.className ="navbar";
 
 var ul = document.createElement("ul");
@@ -38,7 +104,7 @@ main.appendChild(div1);
 
 
 div1.className="flexbox";
-// show cards//
+
 for (var i = 0; i < movies.length; i++) {
 	var para = document.createElement("div");
 	para.className = "allefilme";
@@ -54,12 +120,12 @@ for (var i = 0; i < movies.length; i++) {
 
 
     var node = document.createElement("div");
-    node.innerHTML = `<h1>${movies[i].title}</h1> <br> <p>Director: <br><br>${movies[i].Director}</p> <p>Description: <br><br> ${movies[i].Description}</p> <button> Like 👍 </button> <span>${movies[i].Likes}</span`
+    node.innerHTML = `<h1>${movies[i].title}</h1> <br> <p>Director: <br><br>${movies[i].Director}</p> <p>Description: <br><br> ${movies[i].Description}</p> <button> Like 👍 </button> <span>${movies[i].Likes}</span `
     para.appendChild(node);
-                                                                                                                                                          //Button mit span dann zählt er hoch//
+                                                                                                                                                          //Button mit span dann zählt er hoch//<button> Like 👍 </button> <span>${movies[i].Likes}</span//
 }
 
-// Like Button//
+ //Like Button
 
 function likebutton(){
 buttons = document.querySelectorAll("button");
@@ -83,7 +149,6 @@ buttons = document.querySelectorAll("button");
 
 
 
-
 //footer//
 
 var footer = document.createElement("footer");
@@ -92,14 +157,7 @@ footer.innerHTML = '<footer><img src="" alt=""><p>&copy; Wolfgang Figl - Code Re
 document.body.appendChild(footer);
 
 
-//button//
 
-/*var button = document.getElementById("clickme"),
-  count = 0;
-button.onclick = function() {
-  count += 1;
-  button.innerHTML = "Click me: " + count;
-};*/
 
 
 
